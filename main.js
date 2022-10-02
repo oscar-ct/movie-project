feac69c4eee74217aa44b5b716dc1b6d7fbf3ad3
Oscar Castro <oscar.a.castro818@gmail.com>
2022-10-01 23:17:12 -0500

    feat: successfully pulled data from glitch JSON server, displayed data on D

--- main.js
+++ main.js
@@ -1,57 +1,53 @@
-const glitchJSON = 'https://bow-muddy-polyanthus.glitch.me/movies';
+
+
+//////////////////////////////////////////  OMDb  ////////////////////////////////////////////////////////
+
 const thisIsATest = '0011011000110110001100100011000100110100011001010110001000111001';
-const mapTitleSearchToDiv = (movie) => `<div onclick="getTitle('${movie.imdbID}')">
-            <div>${movie.Title}</div>
-            <div>${movie.Year}</div>
-            <div><img src="${movie.Poster}"></div>
+const mapTitleSearchToDiv = (movie) => `<div onclick="getTitle('${movie.imdbID}')" class="movie-card">
+            <div><img class="img-sm" src="${movie.Poster}"></div>
+            <div class="date">${movie.Year}</div>
         </div>`;
-const mapGlitchTitlesToDiv = (movie) => `<div>
-                <div><img src="${movie.Poster}"></div>
-                <div>${movie.Title}</div>
-                <div>Year: ${movie.Year}</div>
-                <div>Rating: ${movie.Rating}</div>
-                <div>Year: ${movie.Year}</div>
-                <div>Genre: ${movie.Genre}</div>
-                <div>Director: ${movie.Director}</div>
-                <div>Actors: ${movie.Actors}</div>
-                <div>Plot: ${movie.Plot}</div>
-            </div>`;
-
-
 document.querySelector('#searchTitleBtn').addEventListener('click', function () {
     const titleSearched = document.querySelector('#searchTitleInput').value;
     getSearchTitle(titleSearched);
 
 });
-
 function getSearchTitle (search) {
     fetch("https://omdbapi.com/?apikey=" + [money(thisIsATest)] + "&s" + "=" + search, {
     }).then(resp => resp.json()).then(function (data) {
         document.querySelector('#output').innerHTML = data.Search.map(mapTitleSearchToDiv).join('');
-        console.log(data.Search.map(mapTitleSearchToDiv));
+        console.log(data.Search);
     });
 }
-
-
 function getTitle (IdTitle) {
     fetch("https://omdbapi.com/?apikey=" + [money(thisIsATest)] + "&i" + "=" + IdTitle, {
     }).then(resp => resp.json()).then(function (movie) {
-        document.querySelector('#output').innerHTML = `<div>
-                <div><img src="${movie.Poster}"></div>
-                <div>${movie.Title}</div>
-                <div>Year: ${movie.Year}</div>
-                <div>Rating: ${movie.Ratings[0].Value}</div>
-                <div>Year: ${movie.Year}</div>
-                <div>Genre: ${movie.Genre}</div>
-                <div>Director: ${movie.Director}</div>
-                <div>Actors: ${movie.Actors}</div>
-                <div>Plot: ${movie.Plot}</div>
-                <div><button type="button">add to favorites</button></div>
+        document.querySelector('#output').innerHTML = `<div class="movie-card-lg">
+                <div class="img-lg-container">
+                    <div class="img-lg-container2"><img class="img-lg" src="${movie.Poster}"></div>
+                    <div class="movie-details">
+                        <div class="title">${movie.Title}</div>
+                        <div><span class="category">Year Released:</span>${movie.Year}</div>
+                        <div><span class="category">IMDb Rating:</span>${movie.Ratings[0].Value}</div>
+                        <div><span class="category">Genre:</span>${movie.Genre}</div>
+                        <div><span class="category">Director:</span>${movie.Director}</div>
+                        <div><span class="category">Actors:</span>${movie.Actors}</div>
+                        <div><span class="category">Plot:</span>${movie.Plot}</div>
+                        <div><button type="button">add to favorites</button></div>
+                    </div>
+                </div>
             </div>`;
     });
 
 }
 
+//////////////////////////////////////////  Glitch  ////////////////////////////////////////////////////////
+
+const glitchJSON = 'https://bow-muddy-polyanthus.glitch.me/movies';
+const mapGlitchTitlesToDiv = (movie) => `<div onclick="getGlitchTitle('${movie.imdbID}')" class="movie-card" ">
+                <div><img class="img-sm" src="${movie.Poster}"></div>
+                <div style="text-align: center">${movie.Year}</div>
+            </div>`;
 document.querySelector('#favorites').addEventListener('click', function () {
    getGlitchTitles();
 });
@@ -61,9 +57,31 @@ function getGlitchTitles () {
         console.log(data)
     });
 }
+function getGlitchTitle (id) {
+    fetch(glitchJSON).then(resp => resp.json()).then(function (data) {
+        const result = data.find(obj => {
+            return obj.imdbID === id;
+        });
+        console.log(result);
+        document.querySelector('#output').innerHTML = `<div class="movie-card-lg">
+                <div class="img-lg-container">
+                    <div class="img-lg-container2"><img class="img-lg" src="${result.Poster}"></div>
+                    <div class="movie-details">
+                        <div class="title">${result.Title}</div>
+                        <div><span class="category">Year Released:</span>${result.Year}</div>
+                        <div><span class="category">IMDb Rating:</span>${result.Rating}</div>
+                        <div><span class="category">Genre:</span>${result.Genre}</div>
+                        <div><span class="category">Director:</span>${result.Director}</div>
+                        <div><span class="category">Actors:</span>${result.Actors}</div>
+                        <div><span class="category">Plot:</span>${result.Plot}</div>
+                        <div><button type="button">add to favorites</button></div>
+                    </div>
+                </div>
+            </div>`;
+    });
+}
 
-
-
+//////////////////////////////////////////  Helpers  ////////////////////////////////////////////////////////
 
 const money = (test) => {
     let message = "";
